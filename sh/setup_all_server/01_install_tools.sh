68b37ccfe29d3cfcf038acf3f8360d3b3e0fd486
akihiro <takiaki1001e@gmail.com>
2023-11-23 11:37:07 +0900

    ツールインストールシェルにecho追加

--- sh/setup_all_server/01_install_tools.sh
+++ sh/setup_all_server/01_install_tools.sh
@@ -19,19 +19,24 @@ if [ -n "$GO_EXEC_PATH" ]; then
   export PATH="$PATH:$GO_EXEC_PATH"
 fi
 
-# htop
-sudo apt -y install htop
+echo "alpをインストールします！！！！！！！！！！！！！"
 
 # alp
 sudo wget https://github.com/tkuchiki/alp/releases/download/v1.0.12/alp_linux_amd64.tar.gz
 sudo tar -zxvf alp_linux_amd64.tar.gz
 sudo install alp /usr/local/bin/alp
 
+echo "alpをインストール完了しました！！！！！！！！！！！！！"
+echo "pt-query-digestをインストールします！！！！！！！！！！！！！"
+
 # pt-query-digest
 sudo wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
 sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 sudo apt update
 sudo apt install -y percona-toolkit
+
+echo "pt-query-digestをインストール完了しました！！！！！！！！！！！！！"
+echo "ghをインストールします！！！！！！！！！！！！！"
 
 # github cli
 # 参考：https://github.com/cli/cli/blob/trunk/docs/install_linux.md
@@ -41,5 +46,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
   sudo apt update &&
   sudo apt install gh -y
+
+echo "ghをインストール完了しました！！！！！！！！！！！！！"
 
 gh auth login
